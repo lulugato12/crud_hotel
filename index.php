67b014120859e244aa006cdaf2956dd467e9a1ee
Bety <A01732177@itesm.mx>
2019-05-09 21:28:21 -0500

    modificar index

--- index.php
+++ index.php
@@ -1,36 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-	    <meta 	charset="utf-8">
-	    <link   href="css/bootstrap.min.css" rel="stylesheet">
-	    <script src="js/bootstrap.min.js"></script>
+			<meta 	charset="utf-8">
+			<link   href="css/bootstrap.min.css" rel="stylesheet">
+			<script src="js/bootstrap.min.js"></script>
 	</head>
 
 	<body>
-	    <div class="container">
-	    		<div class="row">
-	    			<h3>Ejemplo de operaciones básicas a una tabla de autos</h3>
-	    		</div>
-				<div class="row">
-					<p>
-						<a href="create.php" class="btn btn-success">Agregar un Auto</a>
-					</p>
-					
-					<table class="table table-striped table-bordered">
-			            <thead>
-			                <tr>		                 
-			                	<th>submarcamarca	</th>
-			                	<th>marca 			</th>
-	                        	<th>A/C 			</th>                          		                  
-			                </tr>
-			            </thead>
-			            <tbody>
-			              	<?php 
-						   	include 'database.php';
+		<div class="container">
+			<div class="row">
+				<!-- cargar informacion del hotel -->
+				<h3>Bienvenido al Hotel</h3>
+		</div>
+		<div class="row">
+				<?php
+						/*include 'database.php';
 						   	$pdo = Database::connect();
 						   	$sql = 'SELECT * FROM auto2 natural join marca2 ORDER BY idauto';
 		 				   	foreach ($pdo->query($sql) as $row) {
-								echo '<tr>';							   	
+								echo '<tr>';
 	    					   	echo '<td>'. $row['nombrec'] . '</td>';
 	    					  	echo '<td>'. $row['nombrem'] . '</td>';
 	                            echo '<td>';    echo ($row['ac'])?"SI":"NO"; echo'</td>';
@@ -43,11 +31,9 @@
 	    					   	echo '</td>';
 							  	echo '</tr>';
 						    }
-						   	Database::disconnect();
-						  	?>
-					    </tbody>
-		            </table>
-	    	</div>
-	    </div> <!-- /container -->
+						   	Database::disconnect();*/
+				?>
+			</div>
+		</div>
 	</body>
 </html>
