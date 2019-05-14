dba282532811cf021057e2877408888e3a9d9764
Bety <A01732177@itesm.mx>
2019-05-14 01:08:24 -0500

    mas mamadas

--- clienteDelete.php
+++ clienteDelete.php
@@ -1,39 +1,38 @@
-<?php 
+<?php
 	require 'database.php';
-	$id = 0;	
+	$id = 0;
 	if ( !empty($_GET['id'])) {
-		$id = $_REQUEST['id'];		
+		$id = $_REQUEST['id'];
 	}
-	
+
 	if ( !empty($_POST)) {
-		// keep track post values		
-		$id = $_POST['id'];		
+		// keep track post values
+		$id = $_POST['id'];
 		// delete data
 		$pdo = Database::connect();
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$sql = "DELETE FROM Cliente WHERE ID = ?";
 		$q = $pdo->prepare($sql);
-		$q->execute(array($id));		
+		$q->execute(array($id));
 		Database::disconnect();
-		header("Location: clientes.php");		
-	} 	
+		header("Location: clientes.php");
+	}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 	    <meta 	charset="utf-8">
-	    <link   href=	"css/bootstrap.min.css" rel="stylesheet">
-	    <script src=	"js/bootstrap.min.js"></script>
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 	</head>
 
 	<body>
-	    <div class="container">	    
+	    <div class="container">
 	    	<div class="span10 offset1">
 	    		<div class="row">
 			    	<h3>Eliminar un cliente</h3>
 			    </div>
-			    
+
 			    <form class="form-horizontal" action="clienteDelete.php" method="post">
 		    		<input type="hidden" name="id" value="<?php echo $id;?>"/>
 					<p class="alert alert-error">Estas seguro que quieres eliminar este cliente ?</p>
@@ -42,7 +41,7 @@
 						<a class="btn" href="clientes.php">No</a>
 					</div>
 				</form>
-			</div>					
+			</div>
 	    </div> <!-- /container -->
 	</body>
 </html>
